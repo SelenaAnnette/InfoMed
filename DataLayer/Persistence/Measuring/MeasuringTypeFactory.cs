805be5841bd53bcd7b6d7cb5c4c811b90fa90f61
Gvinar <Gvinar@yandex.ru>
2013-03-27 01:24:11 +0300

    Added changes to factories

--- DataLayer/Persistence/Measuring/MeasuringTypeFactory.cs
+++ DataLayer/Persistence/Measuring/MeasuringTypeFactory.cs
@@ -6,9 +6,14 @@
 
     public class MeasuringTypeFactory
     {
-        public MeasuringType Create(Guid id, string title)
+        public MeasuringType Create(Guid id, string title, string measuring)
         {
-            return new MeasuringType { Id = id, Title = title };
+            return new MeasuringType { Id = id, Title = title, Measuring = measuring, Description = string.Empty };
+        }
+
+        public MeasuringType Create(Guid id, string title, string measuring, string description)
+        {
+            return new MeasuringType { Id = id, Title = title, Measuring = measuring, Description = description };
         }
     }
 }
