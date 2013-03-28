26e0900728d5e011e98a9cc928d815e272486c84
Andrey Glazunov <Gvinar@yandex.ru>
2013-03-28 15:09:48 +0400

    Fixed bug in MedicamentRepository

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -4,6 +4,7 @@
     using System.Linq;
 
     using DataLayer.Persistence.Group;
+    using DataLayer.Persistence.Medicament;
     using DataLayer.Persistence.Person;
     using DataLayer.Persistence.Symptom;
     using DataLayer.Persistence.Measuring;
@@ -14,33 +15,35 @@
     {
         static void Main(string[] args)
         {
-            var personRepo = Binder.NinjectKernel.Get<IPersonRepository>();
+//            var personRepo = Binder.NinjectKernel.Get<IPersonRepository>();
+            var medicamentRepo = Binder.NinjectKernel.Get<IMedicamentRepository>();
+            var medicaments = medicamentRepo.GetAll().ToList();
 //            var personFactory = new PersonFactory();
 //            var person = personFactory.Create(Guid.NewGuid());
 //            person.FirstName = "FirstName";
 //            person.LastName = "LastName";
 //            person.MiddleName = "MiddleName";
 //            personRepo.CreateOrUpdateEntity(person);
-            var foundPerson = personRepo.GetAll().ToList();
+//            var foundPerson = personRepo.GetAll().ToList();
 //            var groupRepo = Binder.NinjectKernel.Get<IGroupRepository>();
 //            var groupFactory = new GroupFactory();
 //            var group = groupFactory.Create(Guid.NewGuid());
 //            group.Name = "Admins";
 //            groupRepo.CreateOrUpdateEntity(group);
 //            var foundGroup = groupRepo.GetEntitiesByQuery(v => v.Name == "Admins").ToList();
-            var symptomRepo = Binder.NinjectKernel.Get<ISymptomRepository>();
+//            var symptomRepo = Binder.NinjectKernel.Get<ISymptomRepository>();
 //            var symptomFactory = new SymptomFactory();
 //            var symptom = symptomFactory.Create(Guid.NewGuid());
 //            symptom.Name = "Silnaya Bol";
 //            symptomRepo.CreateOrUpdateEntity(symptom);
-            var foundSymptoms = symptomRepo.GetAll().ToList();
+//            var foundSymptoms = symptomRepo.GetAll().ToList();
             Console.ReadKey();
-            var measuringTypeRep = Binder.NinjectKernel.Get<IMeasuringTypeRepository>();
-            var measuringType = measuringTypeRep.GetEntitiesByQuery(m => m.Title == "САД").FirstOrDefault();
-            var personMeasuringRep = Binder.NinjectKernel.Get<IPersonMeasuringRepository>();
-            var personMeasuringFactory = new PersonMeasuringFactory();
-            var personMeasuring = personMeasuringFactory.Create(Guid.NewGuid(),measuringType.Id,Guid.NewGuid(),DateTime.Now,120);
-            personMeasuringRep.CreateOrUpdateEntity(personMeasuring);
+//            var measuringTypeRep = Binder.NinjectKernel.Get<IMeasuringTypeRepository>();
+//            var measuringType = measuringTypeRep.GetEntitiesByQuery(m => m.Title == "САД").FirstOrDefault();
+//            var personMeasuringRep = Binder.NinjectKernel.Get<IPersonMeasuringRepository>();
+//            var personMeasuringFactory = new PersonMeasuringFactory();
+//            var personMeasuring = personMeasuringFactory.Create(Guid.NewGuid(),measuringType.Id,Guid.NewGuid(),DateTime.Now,120);
+//            personMeasuringRep.CreateOrUpdateEntity(personMeasuring);
 
         }
     }
