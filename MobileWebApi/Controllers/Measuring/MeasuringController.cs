d12c75a3c998d6717d17531348cd148457392850
Gvinar <Gvinar@yandex.ru>
2013-05-14 23:35:45 +0300

    Adds AddPersonMeasutings action into MeasuringController

--- MobileWebApi/Controllers/Measuring/MeasuringController.cs
+++ MobileWebApi/Controllers/Measuring/MeasuringController.cs
@@ -12,11 +12,24 @@ namespace MobileWebApi.Controllers.Measuring
 
     public class MeasuringController : Controller
     {
+        public class JsonPersonMeasurings
+        {
+            public Guid MeasuringTypeId { get; set; }
+
+            public double Value { get; set; }
+        }
+
         private readonly IMeasuringTypeRepository measuringTypeRepository;
 
-        public MeasuringController(IMeasuringTypeRepository measuringTypeRepository)
+        private readonly IPersonMeasuringRepository personMeasuringRepository;
+
+        private readonly PersonMeasuringFactory personMeasuringFactory;
+
+        public MeasuringController(IMeasuringTypeRepository measuringTypeRepository, IPersonMeasuringRepository personMeasuringRepository)
         {
             this.measuringTypeRepository = measuringTypeRepository;
+            this.personMeasuringRepository = personMeasuringRepository;
+            this.personMeasuringFactory = new PersonMeasuringFactory();
         }
 
         // GET: /Measuring/GetMeasuringTypes
@@ -26,5 +39,29 @@ namespace MobileWebApi.Controllers.Measuring
             return this.Json(this.measuringTypeRepository.GetAll().ToList(), JsonRequestBehavior.AllowGet);
         }
 
+        [HttpPost]
+        public JsonResult AddPersonMeasurings(Guid personId, DateTime sendingDate, List<JsonPersonMeasurings> measurings)
+        {
+            var isSuccessfully = true;
+            var error = string.Empty;
+
+            foreach (var measuring in measurings)
+            {
+                try
+                {
+                    var personMeasuring = this.personMeasuringFactory.Create(
+                    Guid.NewGuid(), measuring.MeasuringTypeId, personId, sendingDate, measuring.Value);
+
+                    this.personMeasuringRepository.CreateOrUpdateEntity(personMeasuring);
+                }
+                catch (Exception ex)
+                {
+                    isSuccessfully = false;
+                    error = ex.Message;
+                }
+            }
+
+            return this.Json(new { IsSuccessfully = isSuccessfully, Error = error }, JsonRequestBehavior.AllowGet);
+        }
     }
 }
