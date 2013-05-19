801c63347c4ac25bc72cb2a8da3ca306991378a2
Gvinar <Gvinar@yandex.ru>
2013-05-19 11:48:34 +0300

    Adds some new features to WebMobileApi

---
+++ MobileWebApi/Controllers/MedicamentController.cs
@@ -0,0 +1,29 @@
+﻿namespace MobileWebApi.Controllers
+{
+    using System;
+    using System.Linq;
+    using System.Web.Mvc;
+
+    using DataLayer.Persistence.Medicament;
+
+    public class MedicamentController : Controller
+    {
+        private readonly IAssignedMedicamentRepository assignedMedicamentRepository;
+
+        public MedicamentController(IAssignedMedicamentRepository assignedMedicamentRepository)
+        {
+            this.assignedMedicamentRepository = assignedMedicamentRepository;
+        }
+
+        // GET: /Medicament/GetAssignedMedicaments?personId=
+        [HttpGet]
+        public JsonResult GetAssignedMedicaments(Guid personId)
+        {
+            var assignedMedicaments =
+                this.assignedMedicamentRepository.GetEntitiesByQuery(
+                    v => v.PersonConsultation.PatientId == personId).Select(v => new { v.MedicamentId, v.PersonConsultationId, v.MedicamentApplicationWayId, v.Dosage, v.Frequency, v.StartDate, v.FinishDate, v.Id });
+
+            return this.Json(assignedMedicaments, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ MobileWebApi/Scripts/MyScripts/TestScript.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function () {
+    var params = {
+        personId: '11111111-1111-1111-1111-111111111111',
+        sendingDateMilliseconds: new Date().getTime(),
+        timezoneOffset: new Date().getTimezoneOffset(),
+        measurings: [{
+            MeasuringTypeId: '11111111-1111-1111-1111-111111111111',
+            Value: 20.5
+        },
+       {
+           MeasuringTypeId: '11111111-1111-1111-1111-111111111111',
+           Value: 20.5
+       }]
+    };
+
+    $.ajax({
+        url: "http://localhost:15695/Measuring/AddPersonMeasurings",
+        type: 'GET',
+        data: params,
+        dataType: 'application/json; charset=utf-8',
+        success: function (response) {
+            console.log(response);
+        }
+    });
+});
