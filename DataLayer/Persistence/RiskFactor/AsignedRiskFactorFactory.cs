c6a4427a34b4558e25383853c7ed8f4123795297
SelenaAnnette <sindianna@yandex.ru>
2013-05-23 22:49:52 +0400

    Conflict during pull from Gvinar

---
+++ DataLayer/Persistence/RiskFactor/AsignedRiskFactorFactory.cs
@@ -0,0 +1,14 @@
+﻿namespace DataLayer.Persistence.RiskFactor
+{
+    using System;
+
+    using Domain.RiskFactor;
+
+    public class AsignedRiskFactorFactory
+    {
+        public AsignedRiskFactor Create(Guid personId, Guid riskFactorId, string measure, double value)
+        {
+            return new AsignedRiskFactor { PersonId = personId, RiskFactorId = riskFactorId, IsActual = true, Measure = measure, Value = value };
+        }
+    }
+}
---
+++ DataLayer/Persistence/RiskFactor/RiskFactorRepository.cs
@@ -0,0 +1,85 @@
+﻿namespace DataLayer.Persistence.RiskFactor
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Data;
+
+    using Domain.RiskFactor;
+
+    public class RiskFactorRepository : IRiskFactorRepository
+    {
+        private readonly string ConnectionString;
+
+        public RiskFactorRepository(string connectionString)
+        {
+            this.ConnectionString = connectionString;
+        }
+
+        public IEnumerable<RiskFactor> GetAll()
+        {
+            var context = new DomainContext(this.ConnectionString);
+            return context.RiskFactors.Include("AssignedRiskFactors");
+        }
+
+        public RiskFactor GetEntityById(Guid id)
+        {
+            using (var context = new DomainContext(this.ConnectionString))
+            {
+                return context.RiskFactors.Include("AssignedRiskFactors").FirstOrDefault(v => v.Id == id);
+            }
+        }
+
+        public IEnumerable<RiskFactor> GetEntitiesByQuery(Func<RiskFactor, bool> query)
+        {
+            if (query == null)
+            {
+                throw new ArgumentNullException("query");
+            }
+
+            using (var context = new DomainContext(this.ConnectionString))
+            {
+                return context.RiskFactors.Include("AssignedRiskFactors").Where(query).ToList();
+            }                                    
+        }
+
+        public RiskFactor CreateOrUpdateEntity(RiskFactor entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity");
+            }
+
+            using (var context = new DomainContext(this.ConnectionString))
+            {                                
+                if (this.GetEntityById(entity.Id) == null)
+                {
+                    context.RiskFactors.Add(entity);
+                }
+                else
+                {
+                    context.Entry(entity).State = EntityState.Modified;
+                }
+
+                context.SaveChanges();
+            }
+
+            return entity;
+        }
+
+        public void DeleteEntity(Guid id)
+        {
+            using (var context = new DomainContext(this.ConnectionString))
+            {
+                var riskFactor = context.RiskFactors.FirstOrDefault(v => v.Id == id);
+                if (riskFactor == null)
+                {
+                    return;
+                }
+
+                context.RiskFactors.Remove(riskFactor);
+                context.SaveChanges();
+            }
+        }
+    }
+}
